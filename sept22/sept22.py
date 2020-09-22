23811d7432fad53cc1b6aa6585a30278e4e5f3a5
JBrozinski <joseph.brozinski@gmail.com>
2020-09-22 13:40:14 -0600

    Debugged function

--- sept22/sept22.py
+++ sept22/sept22.py
@@ -1,9 +1,9 @@
-1st = [10, 15, 3, 7]
-k = 17
-def two_sum(1st, k):
-    for i in range(len(1st)):
-        for j in range(len(1st)):
-             if i !=j and 1st[i] + 1st[j] == k:
+def two_sum(lst, k):
+    for i in range(len(lst)):
+        for j in range(len(lst)):
+             if i !=j and lst[i] + lst[j] == k:
                 return True 
 
     return False 
+
+print two_sum([10, 15, 3, 7], 17)
