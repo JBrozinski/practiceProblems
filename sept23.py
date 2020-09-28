6bf684a8ae314a62e1e0fd7e88b75c0f4eb5ad24
JBrozinski <joseph.brozinski@gmail.com>
2020-09-27 18:43:17 -0600

    updating algorithm for lowest missing positive number

--- sept23.py
+++ sept23.py
@@ -4,13 +4,17 @@
 
 # You can modify the input array in-place.
 
-
+nums = [1,-1,3,4]
 def firstMissingPos(nums):
-    s = set(nums)
-    i = 1
-
-    while i in s:
-        
-        i+=1
-
-    return i 
+    if not nums:
+        return 1
+    for i, num in enumerate(nums):
+        while i + 1 != nums[i] and 0 < nums[i] <= len(nums):
+            v = nums[i]
+            nums[i], nums[v - 1] = nums[v - 1], nums[i]
+            if nums[i] == nums[v - 1]:
+                break
+    for i, num in enumerate(nums, 1):
+        if num != i:
+            return i 
+    return len(nums) + 1
